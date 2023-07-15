c34152a6caf3c8770b55474ebcbb651763596551
Jeremy Klein <jklein24@gmail.com>
2023-07-14 18:20:29 -0700

    Remove wee_alloc because it is not maintained

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,5 @@ use signer::Mnemonic;
 #[cfg(not(target_arch = "wasm32"))]
 use signer::Seed;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[cfg(not(target_arch = "wasm32"))]
 uniffi::include_scaffolding!("lightspark_crypto");
