fb5439295171807bf0ffbc0e1afd19744ac64aed
Zhen Lu <zhenlu@lightspark.com>
2023-08-06 13:16:39 -0700

    Update signer interface (#7)
    * Refactor the signer to take in seed
    
    * Add invoice and commitment point function interfaces.

--- src/signer.rs
+++ src/signer.rs
@@ -1,15 +1,15 @@
 use std::{fmt, str::FromStr};
 
-use bip32::{secp256k1::ecdsa::signature::Signer, Prefix, XPrv, XPub};
+use bip32::{secp256k1::ecdsa::signature::Signer, secp256k1::ecdsa::Signature, Prefix, XPrv, XPub};
 use k256::{
     ecdh,
     elliptic_curve::{generic_array::GenericArray, FieldBytes, PrimeField, Scalar},
     NonZeroScalar, Secp256k1,
 };
-use sha2::{Sha256, Digest};
 use rand_core::OsRng;
-use wasm_bindgen::{JsError, JsValue};
+use sha2::{Digest, Sha256};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{JsError, JsValue};
 
 #[derive(Copy, Clone, Debug)]
 pub enum LightsparkSignerError {
@@ -94,21 +94,43 @@ impl Seed {
 }
 
 #[wasm_bindgen]
-pub struct LightsparkSigner;
+pub struct LightsparkSigner {
+    seed: Seed,
+    node_private_key: XPrv,
+    commitment_seed: Vec<u8>,
+}
 
 #[wasm_bindgen]
 impl LightsparkSigner {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(seed: &Seed) -> Self {
+        let seed = (*seed).clone();
+        let node_private_key =
+            XPrv::derive_from_path(seed.as_bytes(), &"m/0".parse().unwrap()).unwrap();
+        let commitment_seed = Self::build_commitment_seed(&seed);
+        Self {
+            seed,
+            node_private_key,
+            commitment_seed,
+        }
+    }
+
+    fn build_commitment_seed(seed: &Seed) -> Vec<u8> {
+        let mut hasher = Sha256::new();
+        hasher.update(seed.as_bytes());
+        hasher.update(b"commitment seed");
+        hasher.finalize().to_vec()
+    }
+
+    pub fn get_master_public_key(&self) -> Result<String, LightsparkSignerError> {
+        self.derive_public_key("m".to_owned())
     }
 
     pub fn derive_public_key(
         &self,
-        seed: &Seed,
         derivation_path: String,
     ) -> Result<String, LightsparkSignerError> {
         let xprv = self
-            .derive_key(seed, derivation_path)
+            .derive_key(derivation_path)
             .map_err(|e| LightsparkSignerError::Bip32Error(e))?;
         let public_key = xprv.public_key();
         Ok(public_key.to_string(Prefix::XPUB))
@@ -116,45 +138,39 @@ impl LightsparkSigner {
 
     pub fn derive_key_and_sign(
         &self,
-        seed: &Seed,
         message: Vec<u8>,
         derivation_path: String,
         add_tweak: Option<Vec<u8>>,
         mul_tweak: Option<Vec<u8>>,
     ) -> Result<Vec<u8>, LightsparkSignerError> {
-        let signing_key = self.derive_and_tweak_key(seed, derivation_path, add_tweak, mul_tweak)?;
-
-        use bip32::secp256k1::ecdsa::Signature;
-
+        let signing_key = self.derive_and_tweak_key(derivation_path, add_tweak, mul_tweak)?;
         let signature: Signature = signing_key.sign(&message.as_slice());
         Ok(signature.to_bytes().to_vec())
     }
 
-    pub fn ecdh(
-        &self,
-        seed: &Seed,
-        derivation_path: String,
-        public_key: String,
-    ) -> Result<Vec<u8>, LightsparkSignerError> {
-        let xprv = self
-            .derive_key(seed, derivation_path)
-            .map_err(|e| LightsparkSignerError::Bip32Error(e))?;
-        let secret_key = xprv.private_key().as_nonzero_scalar();
+    pub fn ecdh(&self, public_key: String) -> Result<Vec<u8>, LightsparkSignerError> {
+        let secret_key = self.node_private_key.private_key().as_nonzero_scalar();
         let public_key =
             XPub::from_str(&public_key).map_err(|e| LightsparkSignerError::Bip32Error(e))?;
         let shared_secret = ecdh::diffie_hellman(secret_key, public_key.public_key().as_affine());
         Ok(shared_secret.raw_secret_bytes().to_vec())
     }
 
-    pub fn build_commitment_secret(
-        &self,
-        seed: &Seed,
-        idx: u64,
-    ) -> Vec<u8> {
-        let mut res = self.build_commitment_seed(seed);
+    pub fn sign_invoice(&self, unsigned_invoice: String) -> Vec<u8> {
+        let signing_key = self.node_private_key.private_key();
+        let signature: Signature = signing_key.sign(&unsigned_invoice.as_bytes());
+        signature.to_bytes().to_vec()
+    }
+
+    pub fn get_per_commitment_point(&self, channel: u64, per_commitment_point_idx: u64) -> Vec<u8> {
+        unimplemented!()
+    }
+
+    pub fn build_commitment_secret(&self, channel: u64, per_commitment_point_idx: u64) -> Vec<u8> {
+        let mut res = self.commitment_seed.clone();
         for i in 0..48 {
             let bitpos = 47 - i;
-            if idx & (1 << bitpos) == (1 << bitpos) {
+            if per_commitment_point_idx & (1 << bitpos) == (1 << bitpos) {
                 res[bitpos / 8] ^= 1 << (bitpos & 7);
                 res = Sha256::digest(&res).to_vec();
             }
@@ -162,25 +178,14 @@ impl LightsparkSigner {
         res
     }
 
-    fn build_commitment_seed(
-        &self,
-        seed: &Seed,
-    ) -> Vec<u8> {
-        let mut hasher = Sha256::new();
-        hasher.update(seed.as_bytes());
-        hasher.update(b"commitment seed");
-        hasher.finalize().to_vec()
-    }
-
     fn derive_and_tweak_key(
         &self,
-        seed: &Seed,
         derivation_path: String,
         add_tweak: Option<Vec<u8>>,
         mul_tweak: Option<Vec<u8>>,
     ) -> Result<k256::ecdsa::SigningKey, LightsparkSignerError> {
         let xprv = self
-            .derive_key(seed, derivation_path)
+            .derive_key(derivation_path)
             .map_err(|e| LightsparkSignerError::Bip32Error(e))?;
         //unwrap add_tweak and mul_tweak
         if add_tweak.is_some() && mul_tweak.is_some() {
@@ -200,8 +205,8 @@ impl LightsparkSigner {
         }
     }
 
-    fn derive_key(&self, seed: &Seed, derivation_path: String) -> Result<XPrv, bip32::Error> {
-        let xprv = XPrv::derive_from_path(seed.as_bytes(), &derivation_path.parse()?)?;
+    fn derive_key(&self, derivation_path: String) -> Result<XPrv, bip32::Error> {
+        let xprv = XPrv::derive_from_path(&self.seed.as_bytes(), &derivation_path.parse()?)?;
         Ok(xprv)
     }
 
@@ -252,14 +257,14 @@ mod tests {
         let seed_bytes = hex::decode(seed_hex_string).unwrap();
         let seed = Seed::new(seed_bytes);
 
-        let signer = LightsparkSigner;
-        let xprv = signer.derive_key(&seed, "m".to_owned()).unwrap();
+        let signer = LightsparkSigner::new(&seed);
+        let xprv = signer.derive_key("m".to_owned()).unwrap();
         let xprv_string = xprv.to_string(Prefix::XPRV);
         let expected_string = "xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi";
         assert_eq!(xprv_string.as_str(), expected_string);
 
-        let signer = LightsparkSigner;
-        let xprv = signer.derive_key(&seed, "m/0'".to_owned()).unwrap();
+        let signer = LightsparkSigner::new(&seed);
+        let xprv = signer.derive_key("m/0'".to_owned()).unwrap();
         let xprv_string = xprv.to_string(Prefix::XPRV);
         let expected_string = "xprv9uHRZZhk6KAJC1avXpDAp4MDc3sQKNxDiPvvkX8Br5ngLNv1TxvUxt4cV1rGL5hj6KCesnDYUhd7oWgT11eZG7XnxHrnYeSvkzY7d2bhkJ7";
         assert_eq!(xprv_string.as_str(), expected_string);
@@ -271,13 +276,13 @@ mod tests {
         let seed_bytes = hex::decode(seed_hex_string).unwrap();
         let seed = Seed::new(seed_bytes);
 
-        let signer = LightsparkSigner;
-        let public_key_string = signer.derive_public_key(&seed, "m".to_owned()).unwrap();
+        let signer = LightsparkSigner::new(&seed);
+        let public_key_string = signer.derive_public_key("m".to_owned()).unwrap();
         let expected_string = "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8";
         assert_eq!(public_key_string, expected_string);
 
-        let signer = LightsparkSigner;
-        let public_key_string = signer.derive_public_key(&seed, "m/0'".to_owned()).unwrap();
+        let signer = LightsparkSigner::new(&seed);
+        let public_key_string = signer.derive_public_key("m/0'".to_owned()).unwrap();
         let expected_string = "xpub68Gmy5EdvgibQVfPdqkBBCHxA5htiqg55crXYuXoQRKfDBFA1WEjWgP6LHhwBZeNK1VTsfTFUHCdrfp1bgwQ9xv5ski8PX9rL2dZXvgGDnw";
         assert_eq!(public_key_string, expected_string);
     }
@@ -290,8 +295,8 @@ mod tests {
 
         let public_key_string = "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8";
 
-        let signer = LightsparkSigner;
-        let xprv = signer.derive_key(&seed, "m".to_owned()).unwrap();
+        let signer = LightsparkSigner::new(&seed);
+        let xprv = signer.derive_key("m".to_owned()).unwrap();
         let xpub = xprv.public_key();
         assert_eq!(xpub.to_string(Prefix::XPUB), public_key_string);
 
@@ -299,7 +304,7 @@ mod tests {
 
         let message = b"Hello, world!";
         let signature_bytes =
-            signer.derive_key_and_sign(&seed, message.to_vec(), "m".to_owned(), None, None);
+            signer.derive_key_and_sign(message.to_vec(), "m".to_owned(), None, None);
 
         let signature: Signature = Signature::from_slice(&signature_bytes.unwrap()).unwrap();
         assert!(verification_key.verify(message, &signature).is_ok());
@@ -310,20 +315,19 @@ mod tests {
         let seed1_hex_string = "000102030405060708090a0b0c0d0e0f";
         let seed1_bytes = hex::decode(seed1_hex_string).unwrap();
         let seed1 = Seed::new(seed1_bytes);
-        let pub1 = "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8";
 
         let seed2_hex_string = "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542";
         let seed2_bytes = hex::decode(seed2_hex_string).unwrap();
         let seed2 = Seed::new(seed2_bytes);
-        let pub2 = "xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB";
 
-        let signer = LightsparkSigner;
-        let secret_1 = signer
-            .ecdh(&seed1, "m".to_owned(), pub2.to_string())
-            .unwrap();
-        let secret_2 = signer
-            .ecdh(&seed2, "m".to_owned(), pub1.to_string())
-            .unwrap();
+        let signer1 = LightsparkSigner::new(&seed1);
+        let pub1 = signer1.derive_public_key("m/0".to_owned()).unwrap();
+
+        let signer2 = LightsparkSigner::new(&seed2);
+        let pub2 = signer2.derive_public_key("m/0".to_owned()).unwrap();
+
+        let secret_1 = signer1.ecdh(pub2.to_string()).unwrap();
+        let secret_2 = signer2.ecdh(pub1.to_string()).unwrap();
         assert_eq!(secret_1, secret_2);
     }
 
@@ -338,7 +342,11 @@ mod tests {
         let add_tweak = "8be02a96a97b9a3c1c9f59ebb718401128b72ec009d85ee1656319b52319b8ce";
         let add_tweak_bytes = hex::decode(add_tweak).unwrap();
 
-        let signer = LightsparkSigner;
+        let seed_hex_string = "000102030405060708090a0b0c0d0e0f";
+        let seed_bytes = hex::decode(seed_hex_string).unwrap();
+        let seed = Seed::new(seed_bytes);
+
+        let signer = LightsparkSigner::new(&seed);
         let array = GenericArray::from_slice(&base_bytes);
         let key = signer
             .tweak_key(
