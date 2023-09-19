54a141c8813438c9ce31a14f42bfbce09c36950f
Zhen Lu <zhenlu@lightspark.com>
2023-09-19 16:41:53 -0700

    Setup python project

---
+++ lightspark-crypto-python/scripts/generate-linux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+${PYBIN}/python --version
+${PYBIN}/pip install -r requirements.txt
+
+echo "Generating python file..."
+cd ..
+cargo run --bin uniffi-bindgen generate src/lightspark_crypto.udl --language python --out-dir lightspark-crypto-python/src/lightspark_crypto/ --no-format
+
+echo "Generating native binaries..."
+rustup default 1.67.0
+cargo build --profile release-smaller
+
+echo "Copying linux binary..."
+cp target/release-smaller/liblightspark_crypto.so lightspark-crypto-python/src/lightspark_crypto/libuniffi_lightspark_crypto.so
+
+echo "All done!"
