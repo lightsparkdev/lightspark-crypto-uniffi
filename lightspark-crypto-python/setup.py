ef279954b264a3c2f82bf4ac9ec1d1ba52cab6e8
Zhen Lu <zhenlu@lightspark.com>
2023-09-21 12:29:29 -0700

    Revert setup file

--- lightspark-crypto-python/setup.py
+++ lightspark-crypto-python/setup.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python3
+from setuptools import setup
 
-import setuptools
-
-setuptools.setup()
+setup(
+    name="lightspark_crypto_python",
+    version="0.1.0",
+    description="The Python language bindings for lightspark crypto operations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    include_package_data = True,
+    zip_safe=False,
+    packages=["lightspark_crypto"],
+    package_dir={"lightspark_crypto": "./src/lightspark_crypto"},
+    url="https://github.com/lightsparkdev/lightspark-crypto-uniffi",
+    author="Lightspark Group, Inc. <info@lightspark.com>",
+    license="Apache 2.0",
+    has_ext_modules=lambda: True,
+)
