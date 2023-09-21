cdc0a703757c778f510c3ca484ea0cc40050a4c7
Zhen Lu <zhenlu@lightspark.com>
2023-09-21 00:01:07 -0700

    Update generate-windows.sh

--- lightspark-crypto-python/scripts/generate-windows.sh
+++ lightspark-crypto-python/scripts/generate-windows.sh
@@ -9,7 +9,6 @@ cd ..
 cargo run --bin uniffi-bindgen generate src/lightspark_crypto.udl --language python --out-dir lightspark-crypto-python/src/lightspark_crypto/ --no-format
 
 echo "Generating native binaries..."
-rustup default 1.67.0
 rustup target add x86_64-pc-windows-msvc
 cargo build --profile release-smaller --target x86_64-pc-windows-msvc
 
