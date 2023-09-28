63d0858f0c4699731816783bc58960d9c0583c2d
Zhen Lu <zhenlu@lightspark.com>
2023-09-27 22:28:30 -0700

    Try setup docker/buildx on linux

--- lightspark-crypto-python/scripts/generate-linux-arm64.sh
+++ lightspark-crypto-python/scripts/generate-linux-arm64.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-python3 --version
-pip install --user -r requirements.txt
+${PYBIN}/python --version
+${PYBIN}/pip install -r requirements.txt
 
 echo "Generating python file..."
 cd ..
 cargo run --bin uniffi-bindgen generate src/lightspark_crypto.udl --language python --out-dir lightspark-crypto-python/src/lightspark_crypto/ --no-format
 
 echo "Generating native binaries..."
-make build-linux-arm64-static
+docker buildx build -f build.Dockerfile --platform linux/arm64 -o docker-out .
 
-echo "Copying libraries dylib..."
-cp docker-out/target/aarch64-unknown-linux-gnu/release-smaller/liblightspark_crypto.so lightspark-crypto-python/src/lightspark_crypto/libuniffi_lightspark_crypto.so
+echo "Copying linux binary..."
+cp target/release-smaller/liblightspark_crypto.so lightspark-crypto-python/src/lightspark_crypto/libuniffi_lightspark_crypto.so
 
 echo "All done!"
