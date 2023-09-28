4b14b81d85a245e3c54e24c6397969a6e337cb98
Zhen Lu <zhenlu@lightspark.com>
2023-09-27 23:29:18 -0700

    Add arm64 for linux.

--- lightspark-crypto-python/scripts/generate-linux-arm64.sh
+++ lightspark-crypto-python/scripts/generate-linux-arm64.sh
@@ -12,6 +12,6 @@ echo "Generating native binaries..."
 docker buildx build -f build.Dockerfile --platform linux/arm64 -o docker-out .
 
 echo "Copying linux binary..."
-cp target/release-smaller/liblightspark_crypto.so lightspark-crypto-python/src/lightspark_crypto/libuniffi_lightspark_crypto.so
+cp docker-out/target/aarch64-unknown-linux-gnu/release-smaller/liblightspark_crypto.so lightspark-crypto-python/src/lightspark_crypto/libuniffi_lightspark_crypto.so
 
 echo "All done!"
