20c0bccdb7ab340fd9900c2f30f1d72c4e10aabb
Zhen Lu <zhenlu@lightspark.com>
2024-01-02 09:38:33 -0800

    Fix file name for win build. (#47)

--- lightspark-crypto-python/setup.py
+++ lightspark-crypto-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lightspark_crypto_python",
-    version="0.1.5",
+    version="0.1.6",
     description="The Python language bindings for lightspark crypto operations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
