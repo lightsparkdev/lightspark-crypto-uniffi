a5ed87f5192830757331fe317cf7b9b7b382808a
Zhen Lu <zhenlu@lightspark.com>
2024-03-13 21:56:49 -0700

    Build go lib with newest bindgen.

---
+++ lightspark-crypto-go/internal/lightspark_crypto.c
@@ -0,0 +1,8 @@
+#include <lightspark_crypto.h>
+
+// This file exists beacause of
+// https://github.com/golang/go/issues/11263
+
+void cgo_rust_task_callback_bridge_lightspark_crypto(RustTaskCallback cb, const void * taskData, int8_t status) {
+  cb(taskData, status);
+}
