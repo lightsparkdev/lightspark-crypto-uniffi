ecf32f601ed76606acaa86c370d8a6c39742993d
Zhen Lu <zhenlu@lightspark.com>
2024-09-30 14:38:07 -0700

    Update setup.py
    Bump python package version

--- lightspark-crypto-python/setup.py
+++ lightspark-crypto-python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lightspark_crypto_python",
-    version="0.1.7",
+    version="0.2.0",
     description="The Python language bindings for lightspark crypto operations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
